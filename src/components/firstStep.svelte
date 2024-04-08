7436ae0320a019692d66b5bfc4dc8834b9670ef1
Annurkenova <gann3115@gmail.com>
2024-04-09 02:36:53 +0600

    project

---
+++ src/components/firstStep.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  function selectMove(move: string) {
+    dispatch("setpage", { page: 2, move });
+  }
+</script>
+
+<section class="first-step">
+  <label for="paper" class="hand paper">
+    <input type="radio" id="paper" name="move" value="paper"
+      on:change={() => selectMove("paper")} />
+    <img src="./public/images/Paper.png" alt="Paper" class="image" />
+  </label>
+  <label for="scissors" class="hand scissors">
+    <input type="radio" id="scissors" name="move" value="scissors" on:change={() => selectMove("scissors")}
+    />
+    <img src="./public/images/Scissors.png" alt="Scissors" class="image" />
+  </label>
+  <label for="rock" class="hand rock">
+    <input
+      type="radio"
+      id="rock"
+      name="move"
+      value="rock"
+      on:change={() => selectMove("rock")}
+    />
+    <img src="./public/images/Rock.png" alt="Rock" class="image" />
+  </label>
+</section>
+
+<style>
+  .first-step {
+    background-image: url("/public/images/triangle.png");
+    background-repeat: no-repeat;
+    width: 476px;
+    height: 430px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-top: 100px;
+    background-position: center;
+  }
+
+  .scissors {
+    margin-left: 20px;
+  }
+
+  .paper {
+    margin-right: 20px;
+  }
+
+  .first-step .hand {
+    cursor: pointer;
+    transition: all 0.25s;
+  }
+
+  .first-step .hand:hover {
+    transform: translate3d(0px, -8px, 0px);
+  }
+
+  .first-step input[type="radio"] {
+    position: absolute;
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+
+  .first-step .image {
+    cursor: pointer;
+  }
+</style>
---
+++ src/components/secondStep.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+    export let selectedItem:string;
+    const dispatch = createEventDispatcher();
+   
+    function selectMove(move:string) {
+    dispatch('setpage', { page: 3, move }); 
+    }
+</script>
+
+<div class="contest">
+    <p>{selectedItem} </p>
+        <div class="userhand">
+            <h3>YOU PICKED</h3>
+            <div class="handImageContainer">
+                <img src="./public/images/Paper.png" alt="ChooseImage"/>
+            </div>
+            <div class="referee">
+                <div class="decision"><h2>YOU WIN</h2></div>
+                <div class="newGame">PLAY AGAIN</div>
+            </div>
+            <div class="rivalhand">
+                <h3>THE HOUSE PICKED</h3>
+                <div class="handImageContainer">
+                    <img src="./public/images/Rock.png" alt="ChooseImageRival"/>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<style>
+    .contest {
+
+        display: flex;
+        margin-top: 50px;
+    }
+
+.contest img {
+    width: 275px;
+    height: 275px;
+}
+
+.contest > div {
+    flex: 1;
+}
+
+.contest h2 {
+    color: white;
+    font-size: 20px;
+    margin-top: 20px;
+    margin-bottom: 50px;
+    text-align: center;
+}
+
+.contest h3 {
+    color: white;
+}
+
+.newGame {
+    color: white;
+}
+
+.contest .handImageContainer {
+    display:flex;
+    justify-content: center;
+}
+   </style>
+   
